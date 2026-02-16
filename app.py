5f1047940536febb5f554c65e65e67ed726cb330
edwardmalaca16-glitch <your-actual-edwardmalaca16@gmail.com>
2026-02-16 21:23:40 +0800

    Made preview much larger with compact file list

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS  # Add this
 import PyPDF2
 import re
 import os
 
 app = Flask(__name__)
-CORS(app)  # Add this
 
 def clean_text(text):
     """
